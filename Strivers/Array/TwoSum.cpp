45e2f918c88a4f3c4dbc5116c490f79cc89aa4af
tulsi <ttulsigupta836@gmail.com>
2026-09-13 19:57:10 +0530

    dd

--- Strivers/Array/TwoSum.cpp
+++ Strivers/Array/TwoSum.cpp
@@ -1,12 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]+nums[j] == target){
+                    return {i,j};
+                }
+            }
+        }
+        return {-1,-1};
+    }
 int main() {
     int n;
-    cout<<"Enter the size of array: ";
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    cout << "Enter the size of array: ";
+    cin >> n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
     }
+    vector<int> ans = twoSum(nums, 5);
+    cout << ans[0] << " " << ans[1];
     return 0;
 }
