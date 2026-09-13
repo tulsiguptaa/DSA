39c0e4604fa8fac1274452576a370b3e2159407c
tulsi <ttulsigupta836@gmail.com>
2026-09-13 22:56:44 +0530

    Ss

--- Strivers/Array/TwoSum.cpp
+++ Strivers/Array/TwoSum.cpp
@@ -1,5 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// Brute force
+
 vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
         for(int i=0;i<n;i++){
@@ -11,6 +14,23 @@ vector<int> twoSum(vector<int>& nums, int target) {
         }
         return {-1,-1};
     }
+
+// Optimal
+
+vector<int> twoSum_(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int, int> mpp;
+       for(int i = 0; i < nums.size(); i++) {
+        int required = target - nums[i];
+
+        if(mpp.find(required) != mpp.end()) {
+            return {mpp[required], i};
+        }
+
+        mpp[nums[i]] = i;
+    }
+        return {-1,-1};
+}
 int main() {
     int n;
     cout << "Enter the size of array: ";
@@ -19,7 +39,13 @@ int main() {
     for(int i = 0; i < n; i++) {
         cin >> nums[i];
     }
+
+    // Brute
     vector<int> ans = twoSum(nums, 5);
-    cout << ans[0] << " " << ans[1];
+    cout << ans[0] << " " << ans[1]<<"\n";
+
+    // Optimal
+    vector<int> ans1 = twoSum_(nums, 5);
+    cout << ans1[0] << " " << ans1[1];
     return 0;
 }
